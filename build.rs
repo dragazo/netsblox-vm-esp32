8fd68f190a3afc5c725f71b53260ce051a9e0d26
Devin Jean <emailcruzjean@yahoo.com>
2023-04-07 01:04:35 -0500

    finish platform refactor

--- build.rs
+++ build.rs
@@ -1,37 +1,40 @@
 use std::fs::{self, File};
 use std::io::{Write, BufWriter};
-use std::fmt::Write as _;
 
-use serde::Deserialize;
+use tera::{Tera, Context};
 
-#[derive(Debug, Deserialize)]
-#[serde(deny_unknown_fields)]
+use serde::{Serialize, Deserialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(deny_unknown_fields, rename_all(deserialize = "kebab-case"))]
 struct Platform {
     #[serde(default)]
     motors: Vec<Motor>,
 
     #[serde(default)]
-    motor_pairs: Vec<MotorPair>,
+    motor_groups: Vec<MotorGroup>,
 }
 
-#[derive(Debug, Deserialize)]
-#[serde(deny_unknown_fields)]
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(deny_unknown_fields, rename_all(deserialize = "kebab-case"))]
 struct Motor {
     name: String,
     gpio: (usize, usize),
 }
 
-#[derive(Debug, Deserialize)]
-struct MotorPair {
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(deny_unknown_fields, rename_all(deserialize = "kebab-case"))]
+struct MotorGroup {
     name: String,
-    left: String,
-    right: String,
+    motors: Vec<String>,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Necessary because of this issue: https://github.com/rust-lang/cargo/issues/9641
     embuild::build::CfgArgs::output_propagated("ESP_IDF")?;
     embuild::build::LinkArgs::output_propagated("ESP_IDF")?;
+
+    let templates = Tera::new("templates/**/*").unwrap();
 
     // generate static info about this build
     {
@@ -40,62 +43,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     {
-        let platform = {
-            let path = std::env::var("PLATFORM").expect("missing PLATFORM env var (see platforms/ for examples)");
-            let content = fs::read_to_string(path).expect("failed to read PLATFORM toml file");
-            toml::from_str::<Platform>(&content).expect("failed to parse PLATFORM toml file")
+        let mut platform = {
+            let content = fs::read_to_string("platform.toml").expect("failed to read platform.toml");
+            toml::from_str::<Platform>(&content).expect("failed to parse platform.toml")
         };
 
-        let mut ledc_channel = 0;
-
-        let mut objects = String::new();
-        let mut handlers = String::new();
-        let mut syscalls = String::new();
-
-        if !platform.motors.is_empty() {
-            syscalls += " SyscallMenu::Submenu {{ label: \"motors\", content: [\n";
-
-            for motor in platform.motors.iter() {
-                write!(objects, r#"
-                    #[allow(non_snake_case)] let motor_{name} = RefCell::new(MotorController {{
-                        positive: LedcDriver::new(peripherals.ledc.channel{pos_channel}, pwm_timer.clone(), peripherals.pins.gpio{pos_gpio}).unwrap(),
-                        negative: LedcDriver::new(peripherals.ledc.channel{neg_channel}, pwm_timer.clone(), peripherals.pins.gpio{neg_gpio}).unwrap(),
-                    }});"#, name = motor.name, pos_gpio = motor.gpio.0, neg_gpio = motor.gpio.1, pos_channel = ledc_channel, neg_channel = ledc_channel + 1).unwrap();
-                ledc_channel += 2;
-
-                write!(handlers, r#"
-                    "driveMotor{name}" => {{
-                        match args.as_slice() {{
-                            [x] => match x.to_number() {{
-                                Ok(x) => {{
-                                    motor_{name}.borrow_mut().set_power(x).unwrap();
-                                    key.complete(Ok(Intermediate::Json(json!("OK"))));
-                                }}
-                                Err(_) => key.complete(Err(format!("drive{name} expected a number, got {{:?}}", x.get_type()))),
-                            }}
-                            _ => key.complete(Err(format!("drive{name} expected 1 arg, got {{}}", args.len()))),
-                        }}
-                        RequestStatus::Handled
-                    }}"#, name = motor.name).unwrap();
-
-                write!(syscalls, r#"
-                    SyscallMenu::Entry {{ label: "drive{name}" }},
-                "#, name = motor.name).unwrap();
-            }
-
-            for pair in platform.motor_pairs.iter() {
-                write!(handlers, r#"
-                    "drivePair
-                "#, name = pair.name, left = pair.left, right = pair.right).unwrap();
-            }
-
-            syscalls += "] },\n";
-        } else {
-            assert!(platform.motor_pairs.is_empty());
+        for motor in platform.motors.iter() {
+            platform.motor_groups.push(MotorGroup { name: motor.name.clone(), motors: vec![motor.name.clone()] });
         }
 
         let mut f = BufWriter::new(File::create("src/platform.rs").unwrap());
-        writeln!(f, include_str!("templates/platform.rs"), objects = objects, handlers = handlers, syscalls = syscalls).unwrap();
+        templates.render_to("platform.rs", &Context::from_serialize(&platform).unwrap(), &mut f).unwrap();
     }
 
     Ok(())
--- templates/platform.rs
+++ templates/platform.rs
@@ -1,3 +1,5 @@
+#![allow(non_snake_case)]
+
 use std::cell::RefCell;
 use std::sync::Arc;
 use std::rc::Rc;
@@ -5,9 +7,9 @@ use std::rc::Rc;
 use esp_idf_hal::units::FromValueType;
 use esp_idf_hal::modem::Modem;
 
-use netsblox_vm::runtime::{{EntityKind, GetType, System, Value, ErrorCause, Config, Request, RequestStatus, Number}};
-use netsblox_vm::json::{{Json, json}};
-use netsblox_vm::runtime::{{CustomTypes, IntermediateType, Key}};
+use netsblox_vm::runtime::{EntityKind, GetType, System, Value, ErrorCause, Config, Request, RequestStatus, Number};
+use netsblox_vm::json::{Json, json};
+use netsblox_vm::runtime::{CustomTypes, IntermediateType, Key};
 use netsblox_vm::template::SyscallMenu;
 
 use netsblox_vm_esp32::system::EspSystem;
@@ -15,115 +17,147 @@ use netsblox_vm_esp32::system::EspSystem;
 use esp_idf_sys::EspError;
 
 use esp_idf_hal::peripherals::Peripherals;
-use esp_idf_hal::ledc::{{config::TimerConfig, Resolution, SpeedMode, LedcTimerDriver, LedcDriver}};
+use esp_idf_hal::ledc::{config::TimerConfig, Resolution, SpeedMode, LedcTimerDriver, LedcDriver};
 
 // -----------------------------------------------------------------
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum NativeType {{ }}
+pub enum NativeType { }
 
 #[derive(Debug)]
-pub enum NativeValue {{ }}
+pub enum NativeValue { }
 
-impl GetType for NativeValue {{
+impl GetType for NativeValue {
     type Output = NativeType;
-    fn get_type(&self) -> Self::Output {{
+    fn get_type(&self) -> Self::Output {
         unreachable!()
-    }}
-}}
+    }
+}
 
 pub struct EntityState;
-impl<C: CustomTypes<S>, S: System<C>> From<EntityKind<'_, '_, C, S>> for EntityState {{
-    fn from(_: EntityKind<'_, '_, C, S>) -> Self {{
+impl<C: CustomTypes<S>, S: System<C>> From<EntityKind<'_, '_, C, S>> for EntityState {
+    fn from(_: EntityKind<'_, '_, C, S>) -> Self {
         EntityState
-    }}
-}}
+    }
+}
 
-pub enum Intermediate {{
+pub enum Intermediate {
     Json(Json),
     Image(Vec<u8>),
-}}
-impl IntermediateType for Intermediate {{
-    fn from_json(json: Json) -> Self {{
+}
+impl IntermediateType for Intermediate {
+    fn from_json(json: Json) -> Self {
         Self::Json(json)
-    }}
-    fn from_image(img: Vec<u8>) -> Self {{
+    }
+    fn from_image(img: Vec<u8>) -> Self {
         Self::Image(img)
-    }}
-}}
+    }
+}
 
 pub struct C;
-impl CustomTypes<EspSystem<Self>> for C {{
+impl CustomTypes<EspSystem<Self>> for C {
     type NativeValue = NativeValue;
     type EntityState = EntityState;
     type Intermediate = Intermediate;
 
-    fn from_intermediate<'gc>(mc: gc_arena::MutationContext<'gc, '_>, value: Self::Intermediate) -> Result<Value<'gc, Self, EspSystem<Self>>, ErrorCause<Self, EspSystem<Self>>> {{
-        Ok(match value {{
+    fn from_intermediate<'gc>(mc: gc_arena::MutationContext<'gc, '_>, value: Self::Intermediate) -> Result<Value<'gc, Self, EspSystem<Self>>, ErrorCause<Self, EspSystem<Self>>> {
+        Ok(match value {
             Intermediate::Json(x) => Value::from_json(mc, x)?,
             Intermediate::Image(x) => Value::Image(Rc::new(x)),
-        }})
-    }}
-}}
+        })
+    }
+}
 
 // -----------------------------------------------------------------
 
-struct MotorController {{
+struct MotorController {
     positive: LedcDriver<'static>, // they say to use ledc driver for general purpose pwm: https://esp-rs.github.io/esp-idf-hal/esp_idf_hal/ledc/index.html
     negative: LedcDriver<'static>,
-}}
-impl MotorController {{
-    fn set_power(&mut self, power: Number) -> Result<(), EspError> {{
+}
+impl MotorController {
+    fn set_power(&mut self, power: Number) -> Result<(), EspError> {
         let max_input = 255;
         let max_duty = self.positive.get_max_duty() as i32;
         let duty = (power.get() as i32).clamp(-max_input, max_input) * max_duty / max_input;
 
-        if duty >= 0 {{
+        if duty >= 0 {
             self.negative.set_duty(0)?;
             self.positive.set_duty(duty as u32)?;
-        }} else {{
+        } else {
             self.positive.set_duty(0)?;
             self.negative.set_duty((-duty) as u32)?;
-        }}
+        }
 
         Ok(())
-    }}
-}}
+    }
+}
 
 // -----------------------------------------------------------------
 
-pub struct UnusedPeripherals {{
+pub struct UnusedPeripherals {
     pub modem: Modem,
-}}
+}
 
-pub fn get_config(peripherals: Peripherals) -> (UnusedPeripherals, Config<C, EspSystem<C>>, &'static [SyscallMenu<'static>]) {{
-    let pwm_timer_config = TimerConfig {{
+pub fn get_config(peripherals: Peripherals) -> (UnusedPeripherals, Config<C, EspSystem<C>>, &'static [SyscallMenu<'static>]) {
+    let pwm_timer_config = TimerConfig {
         frequency: 20.kHz().into(),
         resolution: Resolution::Bits10,
         speed_mode: SpeedMode::LowSpeed,
-    }};
+    };
     let pwm_timer = Arc::new(LedcTimerDriver::new(peripherals.ledc.timer0, &pwm_timer_config).unwrap());
 
-    {objects}
+    {% set_global ledc_channel = 0 %}
+    {% for motor in motors %}
+    let motor_{{motor.name}} = RefCell::new(MotorController {
+        positive: LedcDriver::new(peripherals.ledc.channel{{ledc_channel + 0}}, pwm_timer.clone(), peripherals.pins.gpio{{motor.gpio[0]}}).unwrap(),
+        negative: LedcDriver::new(peripherals.ledc.channel{{ledc_channel + 1}}, pwm_timer.clone(), peripherals.pins.gpio{{motor.gpio[1]}}).unwrap(),
+    });
+    {% set_global ledc_channel = ledc_channel + 2 %}
+    {% endfor %}
 
-    let config = Config::<C, _> {{
-        request: Some(Rc::new(move |_, _, key, request, _| match &request {{
-            Request::Syscall {{ name, args }} => match name.as_str() {{
-                {handlers}
-                _ => RequestStatus::UseDefault {{ key, request }},
-            }}
-            _ => RequestStatus::UseDefault {{ key, request }},
-        }})),
+    let config = Config::<C, _> {
+        request: Some(Rc::new(move |_, _, key, request, _| match &request {
+            Request::Syscall { name, args } => match name.as_str() {
+                {% for motor_group in motor_groups %}
+                "drive{{motor_group.name}}" => {
+                    match args.as_slice() {
+                        [{% for motor in motor_group.motors %}x_{{motor}},{% endfor %}] => match ({% for motor in motor_group.motors %}x_{{motor}}.to_number(),{% endfor %}) {
+                            ({% for motor in motor_group.motors %}Ok(x_{{motor}}),{% endfor %}) => {
+                                {% for motor in motor_group.motors %}
+                                motor_{{motor}}.borrow_mut().set_power(x_{{motor}}).unwrap();
+                                {% endfor %}
+                                key.complete(Ok(Intermediate::Json(json!("OK"))));
+                            }
+                            _ => key.complete(Err(format!("drive{{motor_group.name}} expected only numeric inputs"))),
+                        }
+                        _ => key.complete(Err(format!("drive{{motor_group.name}} expected {{motor_group.motors|length}} args, got {}", args.len()))),
+                    }
+                    RequestStatus::Handled
+                }
+                {% endfor %}
+                _ => RequestStatus::UseDefault { key, request },
+            }
+            _ => RequestStatus::UseDefault { key, request },
+        })),
         command: None,
-    }};
+    };
 
     let syscalls = &[
-        {syscalls}
+        {% if motor_groups | length > 0 %}
+        SyscallMenu::Submenu {
+            label: "motors",
+            content: &[
+                {% for motor_group in motor_groups %}
+                SyscallMenu::Entry { label: "drive{{motor_group.name}}" },
+                {% endfor %}
+            ],
+        },
+        {% endif %}
     ];
 
-    let unused_peripherals = UnusedPeripherals {{
+    let unused_peripherals = UnusedPeripherals {
         modem: peripherals.modem,
-    }};
+    };
 
     (unused_peripherals, config, syscalls)
-}}
+}
